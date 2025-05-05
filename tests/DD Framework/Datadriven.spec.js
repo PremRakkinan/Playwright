4e197d4cef238ef7c495512ca72faeea47726fdb
PremRakkinan <premkumarrakkinan@gmail.com>
2025-05-05 10:23:08 +0530

    Initl

--- tests/DD Framework/Datadriven.spec.js
+++ tests/DD Framework/Datadriven.spec.js
@@ -19,6 +19,7 @@ testData.forEach((data) => {
     await page.getByRole('textbox', { name: 'Password Password' }).fill(data.Password);
     await page.getByRole('button', { name: 'Log In' }).click();
     await page.pause();
+    //jhdsj
 
   });
 });
